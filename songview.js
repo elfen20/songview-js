aec5878a2341ac2ed888a45b5d3362bb0f7d769f
Elfen20 <elfen20@outlook.de>
2022-08-17 16:49:12 +0200

    wip, parse blocks

--- songview.js
+++ songview.js
@@ -1,19 +1,78 @@
 // songview.js
 // (c) Gernot Lenkner 2022
 
-class SongView {
+class SongView {    
+
     constructor() {
+        this.blocks = [];
+        this.meta = {};
+
+        this.Blocktypes = {
+            Meta: 'Meta',
+            Song: 'Song',
+            Alike: 'Alike',
+            Repeat : 'Repeat',
+        }
     }
 
+    parseBlocks() {
+        let tempBlock = {
+            Type: 'Song',
+            Name: 'Default',
+            Lines: []
+        };
+        let newBlock = {};
+        songview.blocks = [];
+        songview.blocks.push(tempBlock);
+        let lines = songview.textArea.value.split(/\r?\n/);
+        for (let line of lines) {
+            if (line.length > 1) {
+                let control = line.substring(0,2);
+                switch (control) {
+                    case '# ':
+                        // SongBlock
+                        newBlock = {
+                            Type: 'Song',
+                            Name: line.substring(1).trim(),
+                            Lines: []
+                        };
+                        songview.blocks.push(newBlock);
+                        tempBlock = newBlock;
+                        break; 
+                    case '#!':
+                        // MetaBlock
+                        newBlock = {
+                            Type: 'Meta',
+                            Name: line.substring(2).trim(),
+                            Lines: []
+                        };
+                        songview.meta = newBlock;
+                        tempBlock = newBlock;
+                        break;
+                    default:
+                        tempBlock.Lines.push(line.trim());
+                        break;
+                }
+            }
+        }
+    }
+
+    render() {
+
+    }
+
+
     update() {
-       //this.viewer.innerHTML = this.textArea.nodeValue;
-       songview.viewer.innerHTML = songview.textArea.value;
+        //this.viewer.innerHTML = this.textArea.nodeValue;
+        //songview.viewer.innerHTML = songview.textArea.value;
+        songview.parseBlocks();
+        console.debug(songview.blocks);
     }
 
     init() {
         songview.textArea = document.querySelector("#svTextArea");
         songview.viewer = document.querySelector("#svViewer");
-        songview.textArea.addEventListener("keyup", songview.update)
+        songview.textArea.addEventListener("keyup", songview.update)        
         console.debug("SongView Init");
     }
 
